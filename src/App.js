d78a5c3a578ec57819634c54773b8319c11eba0e
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-10-06 00:01:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import Todo from './todo';
 import './App.css';
+import db from './firebase'
 
 function App(){
   const [todos,setTodos]=useState(['TAke dogs for a walk','Take the rubbish out.',' I love to go to party']);
   const[input,setInput]=useState('');
 
+  //when app loads we need to connect to the database and fetch new todos
+  useEffect(()=>{
+    //this code here ... fires when the app.js loads
+    db.collection('Todo').onSnapshot(snapshot=>{
+      console.log(snapshot.docs.map(doc=> doc.data()));
+      setTodos(snapshot.docs.map(doc=> doc.data().value))
+    })
+  }, []);
+
   const addTodo=(event)=>{
     event.preventDefault();
-    console.log(event);
+
+    db.collection('Todo').add({
+      value:input
+    })
+    
     setTodos([...todos,input]);
     setInput('');
-    console.log(todos);
   }
   return (
      <div className="App">
